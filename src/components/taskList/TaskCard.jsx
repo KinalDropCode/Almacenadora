ae1f2e191236f925711cc304730a586b9188b7ed
Informatica <informatica@alumno.local>
2024-05-02 11:54:19 -0600

    Change

--- src/components/taskList/TaskCard.jsx
+++ src/components/taskList/TaskCard.jsx
@@ -1,38 +1,38 @@
-
 import { useNavigate } from "react-router-dom";
-import './task1.css'
+import "./task1.css";
 
 export const TaskCard = ({
-    id,
-    nameTask,
-    endDate,
-    statusTask,
-    author,
-    description,
-    navigateToChannelHandler,
-    
+  id,
+  nameTask,
+  endDate,
+  statusTask,
+  author,
+  description,
+  navigateToChannelHandler,
 }) => {
-    const navigate = useNavigate();
-    
-    const handleNavigate = () => {
-        navigateToChannelHandler(id);
-    };
+  const navigate = useNavigate();
 
-    
-    const statusColor = statusTask === 'Completed' ? 'text-success' : 'text-danger';
+  const handleNavigate = () => {
+    navigateToChannelHandler(id);
+  };
 
-    return (
-        <div className="task-container">
-        <div className="task" style={{ width: '18rem' }}>
-            <div className="card-body">
-                <h5 className="task-title">{nameTask}</h5>
-                <h6 className="task-date">{endDate}</h6>
-                <h6 className={`task-status ${statusColor}`}>{statusTask}</h6>
-                <p className="task-author">{author}</p>
-                <p className="task-description">{description}</p>
-                <button className=" btn-primary" onClick={handleNavigate}>Complete task</button>
-            </div>
+  const statusColor =
+    statusTask === "Completed" ? "text-success" : "text-danger";
+
+  return (
+    <div className="task-container">
+      <div className="task" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="task-title">{nameTask}</h5>
+          <h6 className="task-date">{endDate}</h6>
+          <h6 className={`task-status ${statusColor}`}>{statusTask}</h6>
+          <p className="task-author">{author}</p>
+          <p className="task-description">{description}</p>
+          <button className=" btn-primary" onClick={handleNavigate}>
+            Complete task
+          </button>
         </div>
-    </div>    
-);
+      </div>
+    </div>
+  );
 };
