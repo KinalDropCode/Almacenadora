5bee44de6511fd2878239f82e14fd842a86f23fc
Angel <areyes-2019092@kinal.edu.gt>
2024-05-02 18:42:16 -0600

    New Styles implemented

--- src/components/taskList/TaskCard.jsx
+++ src/components/taskList/TaskCard.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./task1.css";
+import DeleteIcon from '../../assets/img/DeleteIcon.png';
+import EditIcon from '../../assets/img/EditIcon.png';
 
 export const TaskCard = ({
   id,
@@ -12,19 +14,22 @@ export const TaskCard = ({
   onEditTask,
 }) => {
   const [pendingVisible, setPendingVisible] = useState(false);
-  const [deleteEditVisible, setDeleteEditVisible] = useState(true);
+  const [deleteVisible, setDeleteVisible] = useState(true);
   const [completeVisible, setCompleteVisible] = useState(true);
+  const [editVisible, setEditVisible] = useState(true);
 
   const handleCompleteTask = () => {
     setPendingVisible(true);
-    setDeleteEditVisible(false);
+    setDeleteVisible(true);
+    setEditVisible(false); // Ocultar botón de Edit
     setCompleteVisible(false);
   };
 
   const handlePendingTask = () => {
     setPendingVisible(false);
-    setDeleteEditVisible(true);
+    setEditVisible(true); // Mostrar botón de Edit
     setCompleteVisible(true);
+    setDeleteVisible(true);
   };
 
   const handleDelete = () => {
@@ -47,26 +52,31 @@ export const TaskCard = ({
           <h6 className={`task-status ${statusColor}`}>{statusTask}</h6>
           <p className="task-author">{author}</p>
           <p className="task-description">{description}</p>
-          {deleteEditVisible && (
-            <>
-              <button className="btn-delete" onClick={handleDelete}>
-                Delete
-              </button>
-              <button className="btn-edit" onClick={handleEdit}>
-                Edit
-              </button>
-            </>
-          )}
+
           {completeVisible && (
             <button className="btn-primary" onClick={handleCompleteTask}>
               Complete Task
             </button>
           )}
+
           {pendingVisible && (
-            <button className="btn-primary" onClick={handlePendingTask}>
+            <button className="btn-pending" onClick={handlePendingTask}>
               Pending
             </button>
           )}
+
+          {editVisible && (
+            <button className="btn-edit" onClick={handleEdit}>
+              <img src={EditIcon} alt="Edit" />
+            </button>
+          )}
+
+          {deleteVisible && (
+            <button className="btn-delete" onClick={handleDelete}>
+              <img src={DeleteIcon} alt="Delete" />
+            </button>
+          )}
+
         </div>
       </div>
     </div>
